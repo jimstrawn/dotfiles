f163d9e7cd0f1629374ed8946f7f3e3408f85be1
jim.strawn <jdstrawn@gmail.com>
2023-10-11 22:03:14 -0500

    Add initial neovim options

---
+++ lua/options.lua
@@ -0,0 +1,18 @@
+vim.g.mapleader = " "
+
+vim.opt.backup = false
+vim.opt.nu = true
+vim.opt.signcolumn = "yes"
+vim.opt.swapfile = false
+vim.opt.termguicolors = true
+
+-- Formatting
+vim.opt.tabstop = 2
+vim.opt.softtabstop = 2
+vim.opt.shiftwidth = 2
+vim.opt.expandtab = true
+vim.opt.smartindent = true
+
+-- Search
+vim.opt.hlsearch = false
+vim.opt.incsearch = true
