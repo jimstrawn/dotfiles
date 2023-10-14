dedcf616507cd48052510e60ce9f81f27fb23a4b
jim.strawn <jdstrawn@gmail.com>
2023-10-13 20:46:17 -0500

    Install telescope-fzy-native neovim plugin
    This reverts commit 1e3493a5eaf72e2b1ca61fbca67c75dda78a0614.

--- lua/plugins/telescope.lua
+++ lua/plugins/telescope.lua
@@ -1,4 +1,7 @@
 return {
   'nvim-telescope/telescope.nvim', tag = '0.1.4',
-  dependencies = { 'nvim-lua/plenary.nvim' }
+  dependencies = { 'nvim-lua/plenary.nvim' },
+  config = function()
+    require('telescope').load_extension('fzy_native')   
+  end,
 }
