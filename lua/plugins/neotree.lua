4dd26dac1857dfacde3e28636d59ba58f4c2ffb7
jim.strawn <jdstrawn@gmail.com>
2023-10-27 17:08:53 -0500

    Install neo-tree

---
+++ lua/plugins/neotree.lua
@@ -0,0 +1,15 @@
+return {
+  "nvim-neo-tree/neo-tree.nvim",
+  branch = "v3.x",
+  dependencies = {
+    "nvim-lua/plenary.nvim",
+    "nvim-tree/nvim-web-devicons",
+    "MunifTanjim/nui.nvim",
+  },
+  keys = {
+    { "<leader>e", "<cmd>Neotree toggle<cr>", desc = "Open/Close file explorer" }
+  },
+  config = function()
+    require("neo-tree").setup()
+  end,
+}
