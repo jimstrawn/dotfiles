5563e5dadeabec2238b3cf1c611065d6c712bef3
jim.strawn <jdstrawn@gmail.com>
2023-10-30 08:52:15 -0500

    Install mason-lspconfig

--- lua/plugins/lsp.lua
+++ lua/plugins/lsp.lua
@@ -1,8 +1,15 @@
 return {
   {  
-    "williamboman/mason.nvim", 
+    'williamboman/mason.nvim', 
     config = function()
-      require("mason").setup()
-    end
+      require('mason').setup()
+    end,
   },
+  {
+    'williamboman/mason-lspconfig.nvim',
+    config = function()
+      ensure_installed = { 'lua_ls' },
+      require('mason-lspconfig').setup()
+    end,
+  }
 }
