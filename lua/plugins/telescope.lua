ab3ed7dc3e1bf02ee66b15c33ce170664d1e5b61
jim.strawn <jdstrawn@gmail.com>
2023-10-30 21:02:07 -0500

    Add telescope action keybindings

--- lua/plugins/telescope.lua
+++ lua/plugins/telescope.lua
@@ -1,13 +1,33 @@
 return {
   'nvim-telescope/telescope.nvim', tag = '0.1.4',
-  dependencies = { 'nvim-lua/plenary.nvim' },
-  keys = {
-    {'<leader>fb', "<cmd>Telescope buffers<cr>", mode = "n", desc = "Find buffer"},
-    {'<leader>ff', "<cmd>Telescope find_files<cr>", mode = "n", desc = "Find file"},
-    {'<leader>fg', "<cmd>Telescope git_files<cr>", mode = "n", desc = "Find git file"},
-    {'<leader>fs', "<cmd>Telescope live_grep<cr>", mode = "n", desc = "Find string in cwd"},
+  dependencies = { 
+    'nvim-lua/plenary.nvim',
+    'nvim-tree/nvim-web-devicons'
   },
   config = function()
-    require('telescope').load_extension('fzy_native')   
-  end,
+
+    local telescope = require("telescope")
+    local actions = require ("telescope.actions")
+
+    telescope.setup({
+      defaults = {
+        mappings = {
+          i = {
+            ["<C-k>"] = actions.move_selection_previous,
+            ["<C-j>"] = actions.move_selection_next,
+            ["<C-q>"] = actions.send_selected_to_qflist + actions.open_qflist,
+          }
+        }
+      }
+    })
+
+    telescope.load_extension('fzy_native')   
+   
+    local keymap = vim.keymap
+    
+    keymap.set('n', '<leader>fb', "<cmd>Telescope buffers<cr>", { desc = "Find buffer"})
+    keymap.set('n', '<leader>ff', "<cmd>Telescope find_files<cr>", { desc = "Find file"})
+    keymap.set('n', '<leader>fg', "<cmd>Telescope git_files<cr>", { desc = "Find git file"})
+    keymap.set('n', '<leader>fs', "<cmd>Telescope live_grep<cr>", { desc = "Find string in cwd"})
+  end
 }
