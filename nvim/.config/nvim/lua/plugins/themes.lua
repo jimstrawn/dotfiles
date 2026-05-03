5a011fa89c32447ff1ab181b09f4f13bbf956b27
Jim Strawn <jdstrawn@gmail.com>
2026-05-03 12:45:53 -0500

    Add gitsigns neovim plugin

--- nvim/.config/nvim/lua/plugins/themes.lua
+++ nvim/.config/nvim/lua/plugins/themes.lua
@@ -2,6 +2,7 @@ return {
     "rebelot/kanagawa.nvim",
     priority = 1000, -- make sure to load this before all the other start plugins
     config = function()
-    require("kanagawa").load("wave")  end
+        require("kanagawa").load("wave")
+    end
 }
 
