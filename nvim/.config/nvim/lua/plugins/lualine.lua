e34ccf4854a2df19f72806eaa7e60690d5a6e3b5
Jim Strawn <jdstrawn@gmail.com>
2026-05-03 14:45:09 -0500

    Add lualine neovim plugin

---
+++ nvim/.config/nvim/lua/plugins/lualine.lua
@@ -0,0 +1,12 @@
+return {
+    "nvim-lualine/lualine.nvim",
+    dependencies = {
+        "nvim-tree/nvim-web-devicons",
+        "rebelot/kanagawa.nvim",  -- ensures kanagawa loads first
+    },
+    opts = {
+        options = {
+            theme = "kanagawa",
+        },
+    },
+}
