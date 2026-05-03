1629f9e5bfedf93ac9226fe8d9e8668cc3488296
Jim Strawn <jdstrawn@gmail.com>
2026-05-03 14:10:03 -0500

    Add telescope neovim plugin

---
+++ nvim/.config/nvim/lua/plugins/telescope.lua
@@ -0,0 +1,13 @@
+return {
+    "nvim-telescope/telescope.nvim", version = '0.2.2',
+    dependencies = {
+        'nvim-lua/plenary.nvim',
+        { 'nvim-telescope/telescope-fzf-native.nvim', build = 'make' },
+    },
+    keys = {
+        { '<leader>ff', '<cmd>Telescope find_files<cr>', desc = 'Telescope Find files' },
+        { '<leader>fg', '<cmd>Telescope live_grep<cr>',  desc = 'Telescope Live grep' },
+        { '<leader>fb', '<cmd>Telescope buffers<cr>',    desc = 'Telescope Buffers' },
+        { '<leader>fh', '<cmd>Telescope help_tags<cr>',  desc = 'Telescope Help tags' },
+    },
+}
